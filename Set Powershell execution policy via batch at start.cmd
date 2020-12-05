b41f62161210fbe9b5dabf93977743cf0ae7d8bb
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2020-12-05 10:32:49 -0700

    Update Set Powershell execution policy via batch at start.cmd

--- Set Powershell execution policy via batch at start.cmd
+++ Set Powershell execution policy via batch at start.cmd
@@ -1,42 +1,103 @@
 @echo off
 title Set the PowerShell Execution Policy
-color 0a
+color 0c
 mode 1000
 cls
-goto Precheck
+goto Pre
 
-:Precheck
-echo Just a reminder that this script needs to be run as administrator.
-echo If this script already is, then proceed on as normal. 
-echo If not, please close the window and reopen with administrator permissions.
-pause
+:Pre
+echo Note: If you are seeing this the second time, after already giving administrator permissions, this is normal.
+echo In such a case, please enter y and you will be directed to the correct place.
+echo =============================================================================================================
+echo =============================================================================================================
+echo Administrator permissions are needed to change the PowerShell Execution Level.
+echo Allow?
+set /p "choice=(Y/N)?..."
+if /i "%choice%" == "Y" (
+    goto RunAdmin
+) else if /i "%choice%" == "N" (
+    echo You have chosen to not give administrator permissions to the windows command prompt.
+	echo As a result, you will not be able to change the PowerShell execution level.
+	echo Command prompt will close in 15 seconds...
+	timeout 15
+	exit
+) else (
+	echo Your input is not one of the choices. 
+	echo Please try again...
+	pause
+	cls
+	goto Pre
+)
+
+:RunAdmin
+echo =======================================
+echo Running administrator Command Prompt...
+echo =======================================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+echo.
+echo ******************************************
+echo  Invoking UAC for Privilege Escalation...
+echo ******************************************
+echo.
+echo Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+echo args = "ELEV " >> "%vbsGetPrivileges%"
+echo For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+echo args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+echo Next >> "%vbsGetPrivileges%"
+echo UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /b
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+cls
 goto Start
 
 :Start
 echo Which execution level do you want to set on PowerShell?
 echo Choose one of the following:
-echo Restricted = 'RT'
-echo AllSigned = 'AS'
-echo RemoteSigned = 'RS' (recommended)
-echo Unrestricted = 'UR'
-echo Bypass = 'BY' (not recommended)
-set choice=
-set /p choice=You choice?: 
-if NOT '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='RT' goto RT
-if '%choice%'=='rt' goto RT
-if '%choice%'=='AS' goto AS
-if '%choice%'=='as' goto AS
-if '%choice%'=='RS' goto RS
-if '%choice%'=='rs' goto RS
-if '%choice%'=='UR' goto UR
-if '%choice%'=='ur' goto UR
-if '%choice%'=='BY' goto BY
-if '%choice%'=='by' goto BY
-echo "%choice%" is not a valid choice. Please type one of the given two letter shorthands for the execution level(capitalization doesn't matter).
-pause
-cls
-goto Start
+echo Restricted = RT
+echo AllSigned = AS
+echo RemoteSigned = RS (recommended)
+echo Unrestricted = UR
+echo Bypass = BY (not recommended)
+set /p "choice=Your choice?..."
+if /i "%choice%" == "RT" (
+    goto RT
+) 
+else if /i "%choice%" == "AS" (
+    goto AS
+) 
+else if /i "%choice%" == "RS" (
+    goto RS
+) 
+else if /i "%choice%" == "UR" (
+	goto UR
+) 
+else if /i "%choice%" == "BY" (
+	goto BY
+) 
+else (
+	echo "%choice%" is not a valid choice. Please type one of the given two letter shorthands for the execution level(capitalization doesn't matter).
+	pause
+	cls
+	goto Start
+)
 
 :RT
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
